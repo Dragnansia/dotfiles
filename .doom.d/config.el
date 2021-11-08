e3b5830ef4e0f44417e7c8c13150040160fa321b
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-11-08 14:05:42 +0100

    update font and cshap lsp

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -30,7 +30,7 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
-(setq doom-font (font-spec :family "Gintronic" :size 24))
+(setq doom-font (font-spec :family "MonoLisa" :size 20))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -38,8 +38,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
+(setq display-line-numbers-type 'relative)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
